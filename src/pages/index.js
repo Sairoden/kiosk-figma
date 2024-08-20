34d0e110ca774db855896572dc66dee30ae4d4df
Sairoden <odingandarosa@gmail.com>
2024-08-20 11:49:24 +0800

    feat: update documentProcessing--replace all images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
 export { default as LandingPage } from "./LandingPage.jsx";
 export { default as FirstPage } from "./FirstPage.jsx";
 export { default as SecondPage } from "./SecondPage.jsx";
+
+// REGISTRATION
+export { default as Register1 } from "./Register1.jsx";
+export { default as Register21 } from "./Register21.jsx";
+export { default as Register22 } from "./Register22.jsx";
+export { default as Register31 } from "./Register31.jsx";
+export { default as Register32 } from "./Register32.jsx";
+export { default as Register4 } from "./Register4.jsx";
+export { default as Register5 } from "./Register5.jsx";
+export { default as Register6 } from "./Register6.jsx";
