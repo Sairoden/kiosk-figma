38a030b65ae408d3927653f11c88dcc368d1b3ac
Sairoden <odingandarosa@gmail.com>
2024-08-20 09:17:55 +0800

    feat: create tizen--add build and setup

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 function App() {
   return (
     <div style={{ height: "100vh", width: "100vw", margin: 0 }}>
-      <iframe
+      {/* <iframe
         title="Figma Prototype"
         style={{ border: "none", width: "100%", height: "100%" }}
         src="https://www.figma.com/embed?embed_host=share&url=https://www.figma.com/proto/qCpgAKcZWWrdPHRuUOHs1s/FNCC-VMS?node-id=355-2055&t=FmOR0nT6VwMZUaP9-1&scaling=contain"
         allowFullScreen
-      ></iframe>
+      ></iframe> */}
+
+      <h1>Hello World</h1>
     </div>
   );
 }
