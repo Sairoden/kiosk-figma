eac533efae7552ee3ed49325010a84a5f20c4de2
Sairoden <odingandarosa@gmail.com>
2024-08-20 10:46:29 +0800

    feat: requestToVisit--add images

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,16 +1,13 @@
 // REACT
-// import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-
-// STYLES
 
 export default function LandingPage() {
   return (
     <>
-      Hello World{" "}
-      <button as={Link} to="/firstPage">
-        First Page
-      </button>
+      <h1>Hello World</h1>
+      <Link to="/firstPage">
+        <button>First Page</button>
+      </Link>
     </>
   );
 }
