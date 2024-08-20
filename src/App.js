2cbf86b8440a6f584de20b1d4dd969c5fecf652e
Lhara Medina <lharamaemedina@gmail.com>
2024-08-20 09:16:12 +0800

    update update public--add generated Tizen web application files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 
 function App() {
   const figmaPrototypeUrl =
-    "https://www.figma.com/proto/KIq4dbLHt4eiWEmnZZVxBd/Kiosk213213-(Community)?node-id=0-1&t=we0zmgRJUateEwHo-1"; // Your Figma prototype link
+    "https://www.figma.com/proto/qCpgAKcZWWrdPHRuUOHs1s/FNCC-VMS?node-id=372-2103&t=a6ORrD1CquU7zvEr-0&scaling=min-zoom&content-scaling=fixed&page-id=355%3A2055&starting-point-node-id=372%3A2103&show-proto-sidebar=1"; // Your Figma prototype link
 
   return (
     <div style={{ width: "100%", height: "100vh" }}>
